d9f3b1b799fcde637c50ebea9a7a7ac6f42bee7d
goran <goran@bde5ac68-c2e4-480a-bd7c-0859b87ef666>
2011-06-08 11:42:03 +0000

    config-changes (defaultValues)
    git-svn-id: file:///home/node/svn/project_se/goran/EPU_Office@174 bde5ac68-c2e4-480a-bd7c-0859b87ef666

--- src/config/Config.java
+++ src/config/Config.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
+import utils.log.Appender;
 import utils.log.Logger;
 import utils.log.TracingLevel;
 
@@ -19,19 +20,40 @@ import utils.log.TracingLevel;
  * @author Alex
  */
 public class Config {
-    
+
     private Properties config;
     // block-anfang: aus config auslesen!
     private static TracingLevel traceLevel;// = TracingLevel.SEVERE;
     private static boolean stackTrace;// = true;
     private static ArrayList<String> appenderList;
+    private static String defaultDbName = "swe_epu_office";
     private String dbPfad;
-    // block-ende
+    private String defaultAppender = "Console";
+    private String defaultTraceLevel = "SEVERE";
+    private Integer defaultPort = 3306;
+    private String defaultDriver = "com.mysql.jdbc.Driver";
+    private String defaultUser = " ";
+    private String defaultDb = "mySQL";
+    private String defaultIp = "localhost";
+    private String defaultPwd = " ";
 
+    // block-ende
     public Config() {
         config = new Properties();
         try {
             config.load(new FileInputStream("config.properties"));
+            if (config == null) {
+                ArrayList<String> defaultSetting = new ArrayList<String>();
+                defaultSetting.add(defaultDbName);
+                defaultSetting.add(defaultAppender);
+                defaultSetting.add(defaultTraceLevel);
+                defaultSetting.add(defaultPort.toString());
+                defaultSetting.add(defaultDriver);
+                defaultSetting.add(defaultUser);
+                defaultSetting.add(defaultDb);
+                defaultSetting.add(defaultIp);
+                defaultSetting.add(defaultPwd);
+            }
             if (config.getProperty("db").equals("mySQL")) {
                 dbPfad = "jdbc:mysql://";
                 dbPfad += ((config.getProperty("db_ip")) + ":");
@@ -49,40 +71,68 @@ public class Config {
             //System.out.println("WERT VON TRACELEVEL: " + traceLevel.toString());
             stackTrace = Boolean.parseBoolean(config.getProperty("stackTrace"));
             appenderList = loadAppender(config.getProperty("appender"));
+
+
         } catch (IOException ex) {
             Logger.log(Level.SEVERE, Config.class, ex);
         }
     }
-    
+
     public void save(ArrayList<String> newValue) {
         try {
+            int counter = 0;
             config.setProperty("db", newValue.get(0));
             config.setProperty("db_user", newValue.get(1));
             config.setProperty("db_pwd", newValue.get(2));
             config.setProperty("db_ip", newValue.get(3));
             config.setProperty("db_port", newValue.get(4));
+
+            String appender = "";
+            for (String a : appenderList) {
+                if (counter == 0) {
+                    appender += a;
+                    counter = 1;
+                } else {
+                    appender += ";" + a;
+                }
+            }
+            config.setProperty("appender", appender);
+            
+            /*
+             *                 defaultSetting.add(defaultDbName);
+                defaultSetting.add(defaultAppender);
+                defaultSetting.add(defaultTraceLevel);
+                defaultSetting.add(defaultPort.toString());
+                defaultSetting.add(defaultDriver);
+                defaultSetting.add(defaultUser);
+                defaultSetting.add(defaultDb);
+                defaultSetting.add(defaultIp);
+                defaultSetting.add(defaultPwd);
+             */
             config.store(new FileOutputStream("config.properties"), null);
+
+
         } catch (IOException ex) {
             Logger.log(Level.SEVERE, Config.class, ex);
         }
     }
-    
+
     public Properties getProperties() {
         return config;
     }
-    
+
     public static TracingLevel getTraceLevel() {
         return traceLevel;
     }
-    
+
     public static boolean getStackTrace() {
         return stackTrace;
     }
-    
+
     public String getDbPath() {
         return dbPfad;
     }
-    
+
     private ArrayList<String> loadAppender(String property) {
         ArrayList<String> appenderList = new ArrayList<String>();
         StringTokenizer tok = new StringTokenizer(property, ";");
@@ -91,7 +141,7 @@ public class Config {
         }
         return appenderList;
     }
-    
+
     public static ArrayList<String> getAppenderList() {
         return appenderList;
     }
