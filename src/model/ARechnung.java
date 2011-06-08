feb634ff2c3027430f09676d0c55b218e9e27970
goran <goran@bde5ac68-c2e4-480a-bd7c-0859b87ef666>
2011-06-08 09:52:56 +0000

    Ausgangsrechnung Eingangsrechnung
    git-svn-id: file:///home/node/svn/project_se/goran/EPU_Office@169 bde5ac68-c2e4-480a-bd7c-0859b87ef666

---
+++ src/model/ARechnung.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Goran-Goggy
+ */
+public class ARechnung {
+
+    private Kontakt kontakt;
+    private Date datum;
+    private String dummy;
+}
---
+++ src/model/ERechnung.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Goran-Goggy
+ */
+public class ERechnung extends AbstractObject {
+
+    private Kontakt kontakt;
+    private String scanPfad;
+    private Date datum;
+    private Integer preis;
+    private String dummy;
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public Kontakt getKontakt() {
+        return kontakt;
+    }
+
+    public void setKontakt(Kontakt kontakt) {
+        this.kontakt = kontakt;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public void setPreis(int preis) {
+        this.preis = preis;
+    }
+
+    public String getScanPfad() {
+        return scanPfad;
+    }
+
+    public void setScanPfad(String scanPfad) {
+        this.scanPfad = scanPfad;
+    }
+}
